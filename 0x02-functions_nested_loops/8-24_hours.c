300770364dfc6867823530524297bbc02b3f8f15
MARIONCHRISTINE <marionchristine00@gmail.com>
2022-12-13 13:33:01 -0800

    ye

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,39 +1,23 @@
 #include "main.h"
 
-
-
 /**
  * jack_bauer - print the every minute in jack bauer's day
  */
 
-
-
 void jack_bauer(void)
 {
-
 int v = 0;
-
 int x = 0, y = 0, z = 0, m = 0;
-
-
 
 while (v < 1440)
 
 {
-
 _putchar(x + '0');
-
 _putchar(y + '0');
-
 _putchar(':');
-
 _putchar(z + '0');
-
 _putchar(m + '0');
-
 _putchar('\n');
-
-
 
 m++;
 if (m > 9)
