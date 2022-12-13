ad20f6350a6df33ea0ff8088ce6575d5bef9691f
MARIONCHRISTINE <marionchristine00@gmail.com>
2022-12-13 12:22:44 -0800

    life

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,26 +4,15 @@
  * Return: always give 0
  */
 int main(void)
-
 {
-
 	_putchar('_');
-
 	_putchar('p');
-
 	_putchar('u');
-
 	_putchar('t');
-
 	_putchar('c');
-
 	_putchar('h');
-
 	_putchar('a');
-
 	_putchar('r');
-
 	_putchar(10);
 	return (0);
-
 }
